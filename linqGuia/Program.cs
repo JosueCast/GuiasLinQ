079581eebd8500366b028d60ce06e3f5dc1cf73e
JosueCast <126531292+JosueCast@users.noreply.github.com>
2024-09-05 22:12:21 -0600

    Clausula OrderBy y OrderByDescending

--- linqGuia/Program.cs
+++ linqGuia/Program.cs
@@ -117,7 +117,14 @@ ListaHabitantes.Add(new Habitante
     IdCasa = 1
 });
 #endregion
-
+#region OrderBy
+var edadA = ListaHabitantes.OrderBy(x => x.Edad);
+var edadAC = from vt in ListaHabitantes orderby vt.Edad select vt;
+foreach (var edad in edadAC)
+{
+    Console.WriteLine(edad.datosHabitante());
+}
+#endregion
 #region typeOf
 var listaEmpleados = new List<Empleado>() {
     new Medico(){ nombre= "Jorge Casa" },
@@ -128,6 +135,49 @@ var medico = listaEmpleados.OfType<Medico>();
 Console.WriteLine(medico.Single().nombre);
 
 #endregion
+
+#region OrderBy
+var edada = ListaHabitantes.OrderBy(x => x.Edad);
+var edadAc = from vt in ListaHabitantes orderby vt.Edad select vt;
+foreach (var edad in edadAC)
+{
+    Console.WriteLine(edad.datosHabitante());
+}
+var habitantes3 = ListaHabitantes.OrderBy(x => x.Edad).ThenBy(x => x.Nombre);
+
+foreach (var h in habitantes3)
+{
+    Console.WriteLine(h.datosHabitante());
+}
+var lista4 = from h in ListaHabitantes orderby h.Edad, h.Nombre ascending select h;
+
+foreach (var h in lista4)
+{
+    Console.WriteLine(h.datosHabitante());
+}
+var habitantes4 = ListaHabitantes.OrderBy(x => x.Edad).ThenByDescending(x => x.Nombre);
+
+foreach (var h in habitantes4)
+{
+    Console.WriteLine(h.datosHabitante());
+}
+
+#endregion
+#region OrderBYDescending()
+/* var listaEdad = ListaHabitantes.OrderByDescending(x => x.Edad);
+foreach (Habitante h in listaEdad) { 
+    Console.WriteLine(h.datosHabitante());
+}
+Console.WriteLine("-------------------------------------------");
+var ListaEdad2 = from h in ListaHabitantes orderby h.Edad descending select h;
+foreach (Habitante h in ListaEdad2)
+{
+    Console.WriteLine(h.datosHabitante());
+}
+*/
+#endregion
+
+
 #region ElementAt
 var terceraCasa = ListaCasas.ElementAt(2);
 Console.WriteLine($"La tercera casa es {terceraCasa.datosCasa()}");
