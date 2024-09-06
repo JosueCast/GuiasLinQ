ecb3554dfd6132d8f9735617d1eaea3807cad88f
JosueCast <126531292+JosueCast@users.noreply.github.com>
2024-09-05 21:40:18 -0600

    Guia I linq terminada

---
+++ linqGuia/Program.cs
@@ -0,0 +1,169 @@
+﻿// See https://aka.ms/new-console-template for more information
+using linqGuia;
+
+
+Console.WriteLine("Hello, World!");
+
+//LinQ
+
+#region Introduccion 
+/*string[]palabras;
+palabras = new string[] { "gato", "perro", "lagarto", "tortuga", "cocdrilo","serpiente", "123456789" };
+Console.WriteLine("Mas de 5 letras");
+
+List<string> resultado = new List<string>();
+
+foreach (string str in palabras) {
+    if (str.Length >5) {
+        resultado.Add(str);
+    }
+}
+
+foreach (var r in resultado) 
+    Console.WriteLine(r);
+*/
+#endregion
+#region utilizando Linq
+/*Console.WriteLine("-----------------------------------------------------");    
+IEnumerable<string> list = from r in palabras where r.Length > 8 select r;
+foreach(var listado in list)
+    Console.WriteLine(listado);
+Console.WriteLine("-----------------------------------------------------");
+*/
+#endregion
+
+
+#region ListaModelos
+List<Casa> ListaCasas = new List<Casa>();
+List<Habitante> ListaHabitantes = new List<Habitante>();
+#endregion
+#region listaCasa
+ListaCasas.Add(new Casa
+{
+    IdCasa = 1,
+    Direccion = "3 av Norte ArcanCity",
+    Ciudad = "Gothan City",
+    numeroHabitaciones = 20,
+});
+ListaCasas.Add(new Casa
+{
+    IdCasa = 2,
+    Direccion = "6 av Sur SmollVille",
+    Ciudad = "Metropolis",
+    numeroHabitaciones = 5,
+});
+ListaCasas.Add(new Casa
+{
+    IdCasa = 3,
+    Direccion = "Forest Hills, Queens, NY 11375",
+    Ciudad = "New York"
+});
+#endregion
+#region ListaHabitante
+ListaHabitantes.Add(new Habitante
+{
+    IdHabitante = 1,
+    Nombre = "Bruno Diaz",
+    Edad = 36,
+    IdCasa = 1
+});
+ListaHabitantes.Add(new Habitante
+{
+    IdHabitante = 2,
+    Nombre = "Clark Kent.",
+    Edad = 40,
+    IdCasa = 2
+});
+ListaHabitantes.Add(new Habitante
+{
+    IdHabitante = 3,
+    Nombre = "Peter Parker",
+    Edad = 25,
+    IdCasa = 3
+});
+ListaHabitantes.Add(new Habitante
+{
+    IdHabitante = 3,
+    Nombre = "Tia Mey",
+    Edad = 85,
+    IdCasa = 3
+});
+ListaHabitantes.Add(new Habitante
+{
+    IdHabitante = 2,
+    Nombre = "Luise Lain",
+    Edad = 40,
+    IdCasa = 2
+});
+ListaHabitantes.Add(new Habitante
+{
+    IdHabitante = 1,
+    Nombre = "Selina Kyle",
+    Edad = 30,
+    IdCasa = 1
+});
+ListaHabitantes.Add(new Habitante
+{
+    IdHabitante = 1,
+    Nombre = "Alfred",
+    Edad = 65,
+    IdCasa = 1
+});
+ListaHabitantes.Add(new Habitante
+{
+    IdHabitante = 1,
+    Nombre = "Nathan Drake",
+    Edad = 36,
+    IdCasa = 1
+});
+#endregion
+#region SentenciasLinQ
+IEnumerable<Habitante> ListaEdad = from ObjetoProvicional
+                                   in ListaHabitantes
+                                   where ObjetoProvicional.Edad > 40
+                                   select ObjetoProvicional;
+
+foreach (Habitante objetoProcicional2 in ListaEdad)
+{
+    Console.WriteLine(objetoProcicional2.datosHabitante());
+}
+
+//Join
+IEnumerable<Habitante> listaCasaGothan = from objetoTemporalHabitante in ListaHabitantes
+                                         join objetoTemporalCasa in ListaCasas
+                                         on objetoTemporalHabitante.IdCasa equals objetoTemporalCasa.IdCasa
+                                         where objetoTemporalCasa.Ciudad == "Gothan City"
+                                         select objetoTemporalHabitante;
+Console.WriteLine("----------------------------------------------------------------------------------------------");
+foreach (Habitante h in listaCasaGothan)
+{
+    Console.WriteLine(h.datosHabitante());
+}
+//Console.ReadLine();
+#endregion
+#region FirsthAndFirsthOrDefault
+Console.WriteLine("----------------------------------------------------------------------------------------------");
+var primeraCasa = ListaCasas.First(); //esto no es linQ es  una fucnin de los Ienumarabl
+Console.WriteLine(primeraCasa.datosCasa());
+
+//aplicando una restriccion sin restricion lambda
+Habitante personaEdad = (from variableTemporalHabitante in ListaHabitantes where variableTemporalHabitante.Edad > 25 select variableTemporalHabitante).First();
+Console.WriteLine(personaEdad.datosHabitante());
+Console.WriteLine("---------------------------Lo mismo pero con lambdas---------------------------------------------------------");
+var Habitante1 = ListaHabitantes.First(objectTemp => objectTemp.Edad > 25);
+Console.WriteLine(Habitante1.datosHabitante());
+
+// Si no tenemos el elemento que buscamos entoences la sonsulta devolvera una exepcion esto detendra el codigo en su totalidad
+
+//Casa EdadError = (from vCasaTemp in ListaCasas where vCasaTemp.Id >10 select vCasaTemp).First() ;
+//Console.WriteLine(EdadError.dameDatosCasa());
+
+Casa CasaConFirsthOrDedault = ListaCasas.FirstOrDefault(vCasa => vCasa.IdCasa > 200);
+if (CasaConFirsthOrDedault == null)
+{
+    Console.WriteLine("No existe !No hay!");
+    return;
+}
+Console.WriteLine("existe !Si existe!");
+
+#endregion
